8a5dd82df6127a31e7ccec49187149478aa0e6c3
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-10 18:38:48 -1000

    cleaned up solution

--- multiples.js
+++ multiples.js
@@ -3,11 +3,11 @@
  * @param  {Number} n Number that is to be used for calculating multiples up to that Number
  * @return {Number}
  */
-exports.sumOfAMultiple = function( n ) {
-  var sum = 0;
-  var arr = [];
-  for (var i = 1; i < n; i++){
-    if (i%3 === 0 || i%5 === 0){
+exports.sumOfAMultiple = function (n) {
+  let sum = 0;
+
+  for (let i = 1; i < n; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
       sum += i;
     }
   }
